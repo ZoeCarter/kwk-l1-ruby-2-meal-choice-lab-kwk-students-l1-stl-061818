c4f6c7375e08ac184316deccf8e9df6f5c328206
ZoeCarter <zcarter-konate@micds.org>
2018-06-19 15:41:33 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,7 +2,7 @@
 def breakfast 
   food = "frosted flakes"
   time_of_day = "morning"
-  puts "#{time_of_day} is the best time to eat #{food}".
+  puts "#{time_of_day} is the best time to eat #{food}."
 end
 
 breakfast
